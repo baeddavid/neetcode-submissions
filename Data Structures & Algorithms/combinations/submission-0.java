9bd09b1b47e71dfdcfc86ee1e3da5ae0fb80494d
David Bae <baeddavid@gmail.com>
2026-07-02 03:22:26 -0500

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> answer = new ArrayList<>();
+        backtrack(n, k, 1, new ArrayList<>(), answer);
+        return answer;
+    }
+
+    private void backtrack(int n, int k, int start, List<Integer> list, List<List<Integer>> answer) {
+        if(list.size() == k) answer.add(new ArrayList<>(list));
+        for(int i = start; i <= n; i++) {
+            list.add(i);
+            backtrack(n, k, i + 1, list, answer);
+            list.remove(list.size() - 1);
+        }
+    }
+}
