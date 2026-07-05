db93fb27cfc842c0801ca59478e074130a5aeedd
David Bae <baeddavid@gmail.com>
2026-07-05 18:01:53 -0500

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<Integer>());
+        }
+
+        int[] indegree = new int[numCourses];
+
+        for(int[] e : prerequisites) {
+            adj.get(e[1]).add(e[0]);
+            indegree[e[0]]++;
+        }
+
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            List<Integer> adjNode = adj.get(node);
+
+            for(int n : adjNode) {
+                if(--indegree[n] == 0) queue.offer(n);
+            }
+        }
+
+        for(int c : indegree) {
+            if(c != 0) return false;
+        }
+
+        return true;
+    }
+}
