1f636c013af6927c89bdd1bcdd9b5e4b96805ed3
David Bae <baeddavid@gmail.com>
2026-07-05 21:15:01 -0500

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.java
@@ -0,0 +1,25 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        /*
+        We sort by start
+        if at any point i[a, b] and i + 1[c, d] where c < b then return false
+        */
+        intervals.sort((a, b) -> Integer.compare(a.start, b.start));
+        for(int i = 1; i < intervals.size(); i++) {
+            if(intervals.get(i).start < intervals.get(i - 1).end) return false;
+        }
+        return true;
+        
+    }
+}
